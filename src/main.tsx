923c116dd5fbc8febee7a47e6864b003b4f7aeb8
tamerlansarakaev <timasarakaev@gmail.com>
2023-06-28 16:44:27 +0300

    [develop] fix some bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import allReducers from './redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import allReducers from "./redux";
 
 const store = configureStore({
   reducer: allReducers,
 });
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
-
